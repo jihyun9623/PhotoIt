b4b8b25661e53636e983701e0c0dfc6b0ea1ff9a
Patabear <irr2000@paran.com>
2021-07-26 16:53:29 +0900

    BUILD : create new STS PJT

---
+++ backend/photoServer/src/main/java/com/ssafy/PhotoServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoServerApplication.class, args);
+	}
+
+}

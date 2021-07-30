a2b184a0cda8a0e508ab74afe969ced1268dc683
bibibic44 <bibibic@gmail.com>
2021-07-30 10:52:26 +0900

    STYLE: modify double quote to single quote

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Mainpage</router-link> |
-    </div>
+    <div id="nav"><router-link to="/">Mainpage</router-link> |</div>
     <router-view />
   </div>
 </template>

416d8bed93c3891af16d9f363a15fd4c4ede0709
ParkSeRyeong <sr5871@gmail.com>
2021-08-02 14:55:39 +0900

    BEfeat: UserController modify

---
+++ backend/photoServer/src/main/java/com/ssafy/api/response/UserRegisterPostRes.java
@@ -0,0 +1,27 @@
+package com.ssafy.api.response;
+
+import com.ssafy.common.model.response.BaseResponseBody;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegisterPostRes extends BaseResponseBody {
+    public static UserRegisterPostRes of(Integer statusCode, String message){
+        // 1. ResponseBody 객체 res 하나 만들어서
+        UserRegisterPostRes res=new UserRegisterPostRes();
+
+        // 2. BaseResponseBody에서 상속받은 응답상태랑 메세지 설정해주고
+        res.setStatusCode(statusCode);
+        res.setMessage(message);
+
+        return res;
+    }
+
+}

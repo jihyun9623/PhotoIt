fd2f2acfc2bd6408c9a96e88f10e980aabb3fed1
bibibic44 <bibibic@gmail.com>
2021-08-02 11:58:32 +0900

    FEAT: create SearchBar and located on the MainPageIntroduction

---
+++ frontend/src/components/Common/SearchBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <div class="search-style">
+      <select name="region" id="search-region" class="me-2">
+        <option value="all">전체</option>
+      </select>
+      <input
+        type="text"
+        id="integrated-search"
+        v-model="something"
+        placeholder="  김작가 #웨딩 #풍경 (※작가 닉네임은 필수가 아닙니다.)"
+        @keyup.enter="integratedSearch"
+      />
+      <button><i class="fas fa-search"></i></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data() {
+    return {
+      something: null,
+      info: {
+        region: null,
+        keyword: null,
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+.search-style {
+  width: 60%;
+  height: 35px;
+  position: absolute;
+  top: 70%;
+}
+input {
+  width: 80%;
+  height: 100%;
+  border: none;
+  padding-top: 1px;
+  padding-bottom: 0;
+}
+button {
+  background-color: white;
+  border: none;
+  height: 100%;
+  padding-top: 1px;
+}
+select {
+  width: 13%;
+  height: 100%;
+  border: none;
+}
+</style>

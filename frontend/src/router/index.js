2811253c27ddf9eb028d3349d87e89654df16675
bibibic44 <bibibic@gmail.com>
2021-08-03 16:53:05 +0900

    TEST: test SearchRegion

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '@/views/MainPage.vue'
 import Login from '@/views/Login.vue'
+import SearchResult from '@/views/SearchResult.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/search',
+    name: 'SearchResult',
+    component: SearchResult,
   },
 ]
 

0417d36d36bc5c82e8619448f0abcc9feb3ba3e8
bibibic44 <bibibic@gmail.com>
2021-08-03 16:58:36 +0900

    FEAT: add temporary Logo

---
+++ frontend/src/components/Common/Logo.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-link
+    :to="{ name: 'MainPage' }"
+    class="
+      router-link-style
+      d-flex
+      justify-content-start
+      ps-4
+      align-items-center
+    "
+  >
+    <img src="../../assets/images/Logo.png" alt="" class="logo" />
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+}
+</script>
+
+<style scpoed>
+.logo {
+  /* 추후 수정 */
+  height: 45px;
+  z-index: 1;
+}
+.router-link-style {
+  display: inline-block;
+  height: 100%;
+}
+</style>

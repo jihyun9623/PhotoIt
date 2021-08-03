3dc5d1390e9a4c77e5a62ce479484a685d2acd33
bibibic44 <bibibic@gmail.com>
2021-08-03 17:02:36 +0900

    FEAT: add profile picture and logo to MainPageHeader

--- frontend/src/components/MainPage/MainPageHeader.vue
+++ frontend/src/components/MainPage/MainPageHeader.vue
@@ -1,18 +1,26 @@
 <template>
-  <nav class="header pb-2 pt-2">
-    <!-- 프로필, 로고 추가 -->
-    <!-- <h1>네브바</h1> -->
+  <nav
+    class="header pb-2 pt-2 d-flex justify-content-between align-items-center"
+  >
+    <div class="logo-box inline-block">
+      <Logo />
+    </div>
     <SearchBar />
+    <ProfileIconMenu />
   </nav>
 </template>
 
 <script>
 import SearchBar from '@/components/Common/SearchBar'
+import Logo from '@/components/Common/Logo'
+import ProfileIconMenu from '@/components/Common/ProfileIconMenu'
 
 export default {
   name: 'MainPageHeader',
   components: {
     SearchBar,
+    Logo,
+    ProfileIconMenu,
   },
 }
 </script>
@@ -24,5 +32,8 @@ export default {
   background-color: transparent;
   z-index: 1;
   height: 10%;
+}
+.logo-box {
+  height: 100%;
 }
 </style>

db517e2826fd005592dfefea314cf7c6b752174a
bibibic44 <bibibic@gmail.com>
2021-08-07 01:38:28 +0900

    FIX: modify store action method of getProfileNickname (get -> post)

--- frontend/src/store/modules/mainpage.js
+++ frontend/src/store/modules/mainpage.js
@@ -37,7 +37,7 @@ const actions = {
       id: id,
     }
     axios({
-      method: 'get',
+      method: 'post',
       url: `http://i5a108.p.ssafy.io:8080/profile`,
       headers: dispatch('login/getToken', { root: true }),
       data: JSON.stringify(payload),

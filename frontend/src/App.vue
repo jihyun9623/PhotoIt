37527396dfa775f97959060d65b0cb8f70b8670b
bibibic44 <bibibic@gmail.com>
2021-08-11 16:00:48 +0900

    FIX: change call location getRegions and getProfileNickname

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,13 @@
 <script>
 export default {
   name: 'App',
+  computed: {
+    isLoginCheck() {
+      return this.$store.state.login.isLogin
+    },
+  },
   created() {
+    this.$store.dispatch('mainpage/getRegions')
     // 로그인 여부 확인, 로그인 했을 때만 프로필과 닉네임을 요청한다.
     this.$store.dispatch('login/isLoginCheck')
     if (this.$store.state.login.isLogin) {

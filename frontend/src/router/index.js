04c028d4013d3c3d19e8181464e917d83917cb5e
jihyun9623 <jihyun9623@gmail.com>
2021-08-12 10:28:06 +0900

    Feat : FE Favlist Component

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '@/views/MainPage.vue'
 import Login from '@/views/Login.vue'
 import SearchResult from '@/views/SearchResult.vue'
+import FavoriteList from '@/views/FavoriteList.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/search',
     name: 'SearchResult',
     component: SearchResult,
+  },
+  {
+    path: '/favlist',
+    name: 'FavoriteList',
+    component: FavoriteList,
   },
 ]
 

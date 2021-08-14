375b5d89c7314c9fc7b72f24071c98c10056035d
Shin HanGyu <koilwon11@naver.com>
2021-08-15 01:10:16 +0900

    CHORE: MainPage Service Detail view modify

--- backend/photoServer/src/main/java/com/ssafy/api/service/MainPageServiceImpl.java
+++ backend/photoServer/src/main/java/com/ssafy/api/service/MainPageServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 @Service
 @Transactional(readOnly = true)
@@ -174,18 +175,23 @@ public class MainPageServiceImpl implements MainPageService{
     @Override
     @Transactional
     public List<ThumbPhotoIdRes> thumbPhotoIds(String nickName, String thumbnail) {
-        int thumbPhotoIdsSize = 20;
+        int thumbPhotoIdsSize = 4;
         MyStudio myStudio = myStudioRepository.findByNickname(nickName)
                             .orElseThrow(RuntimeException::new);
         List<ThumbPhotoIdRes> thumbPhotoIds = new ArrayList<>();
-        for(Photo p : myStudio.getPhotos()) {
-            if(p.getThumbnail()==thumbnail)
-                continue;
+        Stream<Photo> photos = myStudio.getPhotos().stream().sorted(new Comparator<Photo>() {
+            @Override
+            public int compare(Photo o1, Photo o2) {
+                return o1.getIdx() - o2.getIdx();
+            }
+        });
+        photos.limit(5).forEach(p ->{
             ThumbPhotoIdRes temp = ThumbPhotoIdRes.of(p.getThumbnail(), p.getIdx());
-            thumbPhotoIds.add(temp);
-            if(thumbPhotoIds.size() == thumbPhotoIdsSize)
-                break;
-        }
+            if(p.getThumbnail()!=thumbnail)
+                thumbPhotoIds.add(temp);
+        });
+        if(thumbPhotoIds.size()==5)
+            thumbPhotoIds.remove(4);
         return thumbPhotoIds;
     }
 

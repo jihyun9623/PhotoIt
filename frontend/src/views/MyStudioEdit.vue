45a52b2f48bfde4b33a67b1a4f983d90d9d5ece1
Patabear <irr2000@paran.com>
2021-08-16 18:30:22 +0900

    FEAT : MyStudioEdit skeleton code added

--- frontend/src/views/MyStudioEdit.vue
+++ frontend/src/views/MyStudioEdit.vue
@@ -1,14 +1,17 @@
 <template>
+  <MyStudioHeader />
   <h1>MyStudioEdit</h1>
 </template>
 
 <script>
+import MyStudioHeader from '@/components/MyStudio/MyStudioHeader.vue'
 // import component from "component location"
 
 export default {
   name: 'MyStudioEdit',
   components: {
     // components
+    MyStudioHeader,
   },
 }
 </script>

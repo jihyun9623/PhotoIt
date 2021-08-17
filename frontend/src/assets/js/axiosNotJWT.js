0dc1986a034a582596f603953de5d2cd66d639ec
Patabear <irr2000@paran.com>
2021-08-17 23:46:50 +0900

    FEAT : axios No JWT js added

--- frontend/src/assets/js/axiosNotJWT.js
+++ frontend/src/assets/js/axiosNotJWT.js
@@ -4,8 +4,4 @@ const serverURL = 'http://localhost:8080'
 // axios 객체 생성
 export default axios.create({
   baseURL: serverURL,
-  headers: {
-    Authorization: localStorage.getItem('jwt'),
-  },
-  withCredentials: true,
 })

66d7e85eaba9943cd0e31b6021c77ca84f161bf8
Patabear <irr2000@paran.com>
2021-08-19 01:16:28 +0900

    FIX : bugfix

--- backend/photoServer/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/photoServer/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -23,7 +23,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
                 .allowedOrigins("http://localhost:8081")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")

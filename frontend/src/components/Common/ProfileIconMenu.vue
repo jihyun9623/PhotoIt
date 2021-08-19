b4d001f62f279ae1bf7402b95062120996a8f57a
ParkSeRyeong <sr5871@gmail.com>
2021-08-20 04:24:13 +0900

    FIX: CSS_mystudioEdit

--- frontend/src/components/Common/ProfileIconMenu.vue
+++ frontend/src/components/Common/ProfileIconMenu.vue
@@ -49,7 +49,7 @@
           v-if="role === 'PG'"
           :to="{ path: `/mystudioedit/${nickname}` }"
         >
-          마이스튜디오에딧
+          마이스튜디오 EDIT
         </router-link>
       </li>
       <!-- <li v-if="isLogin" class="mb-2"><router-link :to="{ name: '' }"> 찜 목록 </router-link></li> -->
